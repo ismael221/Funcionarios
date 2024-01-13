d4bcd073e717658d059efe3e3e3be86774286df9
Ismael Nunes Campos <ismaeldenunes@gmail.com>
2024-01-12 22:28:23 -0300

    [ADD] Funcionario controller,service e repository implementados

--- src/main/java/com/api/senac/controller/FuncionarioController.java
+++ src/main/java/com/api/senac/controller/FuncionarioController.java
@@ -5,9 +5,7 @@ import com.api.senac.service.FuncionarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,5 +21,27 @@ public class FuncionarioController {
         return new ResponseEntity<>(funcionarios, HttpStatus.OK);
     }
 
+    @GetMapping("/pesquisar/{id}")
+    public ResponseEntity<FuncionarioEntity> getFuncionarioById(@PathVariable Integer id){
+        FuncionarioEntity funcionario = funcionarioService.getFuncionarioId(id);
+        return new ResponseEntity<>(funcionario,HttpStatus.OK);
+    }
 
+    @PostMapping("/adicionar")
+    public ResponseEntity<FuncionarioEntity> addFuncionario(@RequestBody FuncionarioEntity func){
+        var novoFuncionario = funcionarioService.criarFuncionario(func);
+        return new ResponseEntity<>(novoFuncionario,HttpStatus.CREATED);
+    }
+
+    @PutMapping("/atualizar/{id}")
+    public ResponseEntity<FuncionarioEntity> atualizarFuncionario(@PathVariable Integer id,@RequestBody FuncionarioEntity funcionario){
+        var funcionarioAtualizado = funcionarioService.atualizarFuncionario(id,funcionario);
+        return new ResponseEntity<>(funcionarioAtualizado,HttpStatus.OK);
+    }
+
+    @DeleteMapping("/deletar/{id}")
+    public ResponseEntity deletarFuncionario(@PathVariable Integer id){
+        funcionarioService.deletarFuncionario(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
